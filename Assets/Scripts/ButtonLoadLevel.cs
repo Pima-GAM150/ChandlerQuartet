7f857a5f89edc562297932d86a848c7e0685f4b6
LAPTOP-UHC40UK6\chand <chosteler@mail.pima.edu>
2017-12-14 16:22:58 -0700

    Title screen and level 9

---
+++ Assets/Scripts/ButtonLoadLevel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ButtonLoadLevel : MonoBehaviour {
+
+    public int CurrentLevel = 0;
+    public static int maxlevel = 100;
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    private void TaskOnClick()
+    {
+        if (CurrentLevel < maxlevel)
+            {
+                SceneManager.LoadScene(CurrentLevel++);
+            }
+    }
+}
+
+    
